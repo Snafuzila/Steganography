717c8b846a735b5d12eb75428d3c13726ddd7d1a
Snafuzila <Boazattiya8@gmail.com>
2025-08-02 18:52:38 +0300

    unite lsb_img functions to one file and added lsb_vid to hundle encryption in video files .avi .mp4

--- lsb_img.py
+++ lsb_img.py
@@ -71,3 +71,35 @@ def lsb_img_hide_text_with_length(image: Image.Image, message: str) -> Image.Ima
     new_img.putdata(new_pixels)  # Update the pixels with the modified ones
 
     return new_img  # Return the new image (not saved to file yet)
+
+
+
+# Converts a binary string into a text string
+def binary_to_text(binary_string):
+    chars = [binary_string[i:i+8] for i in range(0, len(binary_string), 8)]
+    return ''.join(chr(int(char, 2)) for char in chars)
+
+# Function to extract the hidden message from the image
+def lsb_img_extract_text_from_image(image: Image.Image) -> str:
+    # Make sure the image is in RGB mode
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+
+    pixels = list(image.getdata())
+    bit_stream = ''
+
+    # Step 1: Extract LSBs from all RGB channels
+    for pixel in pixels:
+        for value in pixel:  # r, g, b values
+            bit_stream += str(value & 1)
+
+    # Step 2: First 32 bits represent the length of the message in bits
+    length_bits = bit_stream[:32]
+    message_length = int(length_bits, 2)
+
+    # Step 3: Extract the actual message bits using the length
+    message_bits = bit_stream[32:32+message_length]
+
+    # Step 4: Convert the bits into text
+    hidden_message = binary_to_text(message_bits)
+    return hidden_message
